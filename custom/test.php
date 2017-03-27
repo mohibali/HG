1b4a494c29a43015f36071250655f1d36165787c
sda-PC\sda <riaz.1march@gmail.com>
2017-03-28 02:29:28 +0500

    no message

---
+++ custom/test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sda
+ * Date: 3/19/2017
+ * Time: 3:32 AM
+ */
+
+class test{
+   // public $yesrword="love";
+    public $colour="RED";
+}
+
+
+$myTest =new test();
+echo $myTest->colour;
+
+
+
+?>
